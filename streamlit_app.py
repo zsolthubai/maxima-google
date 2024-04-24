86d584b6efa764b1b2c96ccb888709744d3505eb
Zsolt Hubai <zhubai@travix.com>
2024-04-24 21:04:58 +0200

    change streamlit app to bq query

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,37 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
-st.title('🦜🔗 Quickstart App')
+client = bigquery.Client()
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+# patient_name = st.sidebar.text_input('Patient name')
+# patient_number = st.sidebar.text_input('Patient number', type='password')
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+state_name = st.sidebar.text_input('US state:')
 
-with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-    submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+sql = f"""
+SELECT subpopulation, avg(value) as avg_value FROM `bigquery-public-data.america_health_rankings.ahr` where
+lower(state_name) = "{state_name.lower()}"
+group by 1
+having subpopulation is not null"""
+
+    
+if st.button("Process"):
+    
+    rows = run_query(sql)
+     
+    st.write(sql)
+    # Print results.
+    st.write("Results:")
+    for row in rows:
+        st.write(row['subpopulation'] + "✍️ " + str(row['avg_value']))
